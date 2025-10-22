0424ebb0142876efe686f1b1abe5810a3c7ce192
Emanuele Vittorio <80417622+Emanuelele@users.noreply.github.com>
2025-10-22 23:19:07 +0200

    asd

--- client/ui.lua
+++ client/ui.lua
@@ -27,3 +27,33 @@ RegisterNUICallback('close', function(data, cb)
     SetNuiFocus(false, false)
     cb('ok')
 end)
+
+RegisterNUICallback('purchaseTree', function(treeId, cb)
+    local success = lib.callback.await('peakville_skills:purchaseTree', false, treeId)
+    cb(success)
+end)
+
+RegisterNUICallback('refundTree', function(treeId, cb)
+    local success = lib.callback.await('peakville_skills:refundTree', false, treeId)
+    cb(success)
+end)
+
+RegisterNUICallback('purchaseSkill', function(skillId, cb)
+    local success = lib.callback.await('peakville_skills:purchaseSkill', false, skillId)
+    cb(success)
+end)
+
+RegisterNUICallback('refundSkill', function(skillId, cb)
+    local success = lib.callback.await('peakville_skills:refundSkill', false, skillId)
+    cb(success)
+end)
+
+RegisterNUICallback('selectQuest', function(questId, cb)
+    local success = lib.callback.await('peakville_skills:selectQuest', false, questId)
+    cb(success)
+end)
+
+RegisterNUICallback('deselectQuest', function(questId, cb)
+    local success = lib.callback.await('peakville_skills:deselectQuest', false, questId)
+    cb(success)
+end)
